a30fc09153bf4ca033d74823261fc2453d3a79fe
verget <verget49@gmail.com>
2022-08-25 22:24:30 +0200

    another try

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const telegramToken = process.env.TELEGRAM_KEY;
+const telegramToken = process.env.TELEGRAM;
 
 const request = require('request');
 
@@ -11,9 +11,9 @@ let bot,
 
 if (process.env.NODE_ENV === 'production') {
   bot = new Bot(telegramToken);
-  console.log(process.env.TELEGRAM_KEY)
-  console.log('Setting webhook on ', process.env.EXTERNAL_URL + process.env.TELEGRAM_KEY)
-  bot.setWebHook(process.env.EXTERNAL_URL + process.env.TELEGRAM_KEY);
+  console.log(telegramToken)
+  console.log('Setting webhook on ', `${process.env.URL}/${telegramToken}`)
+  bot.setWebHook(`${process.env.URL}/${telegramToken}`);
 } else {
   bot = new Bot(telegramToken, { polling: true });
 }
