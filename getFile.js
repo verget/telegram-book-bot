f30c1a8ec41336603dd0b7a22121e4b4110d412c
verget <verget49@gmail.com>
2022-09-03 17:24:30 +0200

    Final adjustments

--- getFile.js
+++ getFile.js
@@ -5,7 +5,6 @@ module.exports = async (file_id) => {
     const response = await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getFile`, {
       file_id
     });
-    const file_name = response?.data?.result?.file_name
     const path = response?.data?.result?.file_path
     if (path) {
       const fileResponse = await axios.get(`https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${path}`);
@@ -15,6 +14,4 @@ module.exports = async (file_id) => {
   } catch (error) {
     throw error
   }
-};
-
-//https://api.telegram.org/file/bot<token>/<file_path>
+}
--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -3,11 +3,11 @@ const sendEmail = require("../../sendEmail")
 const getFile = require("../../getFile")
 
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body)
+  console.log("Message received ", event.body)
   const { message } = JSON.parse(event.body);
   await sendMessage(message.chat.id, "I got your message!");
   if (message.from.username === 'to_verge') { // hardcoded because only I and my wife use this bot =)
-    mailTo = 'verget49@gmail.com' // verget49+amazon_sorpme@kindle.com
+    mailTo = 'verget49+amazon_sorpme@kindle.com' // verget49+amazon_sorpme@kindle.com
   } else if (message.from.username === 'doesitoffendme') {
     mailTo = 'margaritasinyuk_K64wlW@kindle.com'
   } else {
@@ -16,8 +16,10 @@ exports.handler = async (event) => {
   if (message.document) {
     try {
       const fileData = await getFile(message.document.file_id)
+      console.log('File path received')
       if (fileData) {
         await sendEmail(mailTo, message.document.file_name, fileData)
+        console.log(`Book ${message.document.file_name} was send to ${mailTo}`)
         await sendMessage(message.chat.id, `Book ${message.document.file_name} was send to ${mailTo}`);
       }
     } catch (error) {
