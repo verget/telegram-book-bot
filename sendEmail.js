6dde880fe1c3156e84c73712285adf367adb4f6b
verget <verget49@gmail.com>
2022-11-03 16:05:14 +0100

    Remove debug log

--- sendEmail.js
+++ sendEmail.js
@@ -10,8 +10,6 @@ module.exports = async (mailTo, file_name, file_content) => {
       pass: process.env.MAIL_PASS
     }
   });
-  console.log(process.env.MAIL_USER)
-  console.log(process.env.MAIL_PASS)
 
   let message = {
     from: `"Book Sender" <${process.env.MAIL_BOX}>`, // sender address
